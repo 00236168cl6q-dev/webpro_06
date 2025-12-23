522f368f5236560e37ba1c8698cb4de9eee443e6
Naruke Kaito <00236168cl6q@gmail.com>
2025-12-23 18:00:15 +0900

    レポート用の作成データベースの案を二つ変更　phigrosのデータベースプログラムは完成させた

---
+++ report.js
@@ -0,0 +1,130 @@
+"use strict";
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use("/public", express.static(path.join(__dirname + "/public")));
+app.use(express.urlencoded({ extended: true }));
+
+let phigros = [
+  { name:"Rrhar'il", level:17.6, author_music:"Team Grimoire", author_notes:"RWND -p 16493:62786:92551", bpm:"190bpm", notes_figure:"1300ノーツ", time:"132秒" ,notes_density:"9.85ノーツ/秒", image:"rrharil.jpg.webp"},
+  { name:"Distorted Fate", level:17.4, author_music:"Sakuzyo", author_notes:"unDefined Future", bpm:"150bpm", notes_figure:"1283ノーツ", time:"162秒", notes_density:"7.92ノーツ/秒", image:"Distorted_Fate.jpg.webp"},
+  { name:"Igallta", level:17.4, author_music:"Se-U-Ra", author_notes:"断罪の剣『神』", bpm:"230bpm", notes_figure:"1114ノーツ", time:"122秒", notes_density:"9.13ノーツ/秒", image:"Igallta_1.jpg.webp"},
+  { name:"QZKago Requiem", level:17.4, author_music:"t+pazolite", author_notes:"Salt feat. M3ow", bpm:"257~290bpm", notes_figure:"1723ノーツ", time:"138秒", notes_density:"12.49ノーツ/秒", image:"QZKago_Requiem.webp.webp"},
+  { name:"+ERABY+E CONNEC+10N", level:17.3, author_music:"かめりあ", author_notes:"2B45524142592B45", bpm:"230bpm", notes_figure:"1579ノーツ", time:"167秒", notes_density:"9.46ノーツ/秒", image:"tera.jpg.webp"},
+  { name:"DESTRUCTION 3,2,1", level:17.3, author_music:"Normal1zer vs. Broken Nerdz", author_notes:"∅", bpm:"321.321bpm", notes_figure:"2330ノーツ", time:"160秒", notes_density:"14.56ノーツ/秒", image:"DESTRUCTION3,2,1.jpg.webp"},
+  { name:"祈 -我ら神祖と共に歩む者なり-", level:17.3, author_music:"光吉猛修 VS 穴山大輔 VS Kai VS 水野健治 VS 大国奏音", author_notes:"フィグロス譜面チーム \"Rikko feat. Ner\" .Pray for \"PHI\".", bpm:"219~270bpm", notes_figure:"2222ノーツ", time:"180秒", notes_density:"12.34ノーツ/秒", image:"祈 -我ら神祖と共に歩む者なり-.webp"},
+  { name:"AbsoluTe disoRdeR", level:17.2, author_music:"Acute Disarray", author_notes:"Apocalyptic Dissonance", bpm:"256bpm", notes_figure:"2025ノーツ", time:"190秒", notes_density:"10.66ノーツ/秒", image:"AbsoluTe disoRdeR.webp"},
+  { name:"Stardust:RAY", level:17.2, author_music:"kanone vs. BlackY", author_notes:"完璧 vs 癒株 -Autumn Memory-", bpm:"164bpm", notes_figure:"1570ノーツ", time:"165秒", notes_density:"9.52ノーツ/秒", image:"Stardust.webp"},
+  { name:"Hydra", level:17.1, author_music:"James Landino X Akira Complex", author_notes:"[该用户已被禁言]", bpm:"160bpm", notes_figure:"1547ノーツ", time:"150秒", notes_density:"10.31ノーツ/秒", image:"Hydra.jpg.webp"},
+  { name:"幻影鬼魅 (PLEASE)", level:17.0, author_music:"R300K", author_notes:"imd visual charter \"Rikko\" since 2014", bpm:"161bpm", notes_figure:"1145ノーツ", time:"125秒", notes_density:"9.16ノーツ/秒", image:"PLEASE.webp"},
+  { name:"BANGING STRIKE", level:16.9, author_music:"Dew Pleiades", author_notes:"魔王、大咩兔の兄『大咩免』!!", bpm:"234bpm", notes_figure:"1677ノーツ", time:"147秒", notes_density:"11.41ノーツ/秒", image:"BANGING_STRIKE.webp"},
+  { name:"Der Richter", level:16.9, author_music:"Ωμεγα", author_notes:"Concat(Ramification,Richter)", bpm:"192bpm", notes_figure:"1306ノーツ", time:"147秒", notes_density:"8.88ノーツ/秒", image:"Der_Richter.webp"},
+  { name:"Re：End of a Dream", level:16.9, author_music:"uma vs. モリモリあつし", author_notes:"XMT, Encore after the end", bpm:"212bpm", notes_figure:"1377ノーツ", time:"141秒", notes_density:"9.77ノーツ/秒", image:"Re_End_of_a_Dream.webp"},
+  { name:"Cuvism³", level:16.8, author_music:"Fl00t vs Halv", author_notes:"JKy vs StrayCat", bpm:"194bpm", notes_figure:"1088ノーツ", time:"160秒", notes_density:"6.80ノーツ/秒", image:"Cuvism.webp"},
+  { name:"INFiNiTE ENERZY -Overdoze-", level:16.8, author_music:"Reku Mochizuki", author_notes:"while(TangScend=TangScend);", bpm:"180bpm", notes_figure:"888ノーツ", time:"120秒", notes_density:"7.40ノーツ/秒", image:"INFiNiTE_ENERZY.webp"},
+  { name:"sølips", level:16.8, author_music:"rintaro soma", author_notes:"ø", bpm:"199bpm", notes_figure:"1175ノーツ", time:"146秒", notes_density:"8.05ノーツ/秒", image:"solips.webp"},
+  { name:"Diamond Dust", level:16.7, author_music:"Masahiro \"Godspeed\" Aoki", author_notes:"StupiGGy", bpm:"200bpm", notes_figure:"1518ノーツ", time:"155秒", notes_density:"9.79ノーツ/秒", image:"Diamond_Dust.webp"},
+  { name:"Spasmodic", level:16.7, author_music:"姜米條☆颶風~♫元力上人♫", author_notes:"無極☆雷鳴~♫死の序曲♫", bpm:"200bpm", notes_figure:"1671ノーツ", time:"155秒", notes_density:"10.78ノーツ/秒", image:"Spasmodic.webp"},
+  { name:"Stasis", level:16.7, author_music:"Maozon", author_notes:"百九十八", bpm:"180bpm", notes_figure:"1700ノーツ", time:"155秒", notes_density:"10.97ノーツ/秒", image:"Stasis.webp"},
+  { name:"ATHAZA", level:16.6, author_music:"LeaF", author_notes:"FORGET :: CLUTTER", bpm:"240~280bpm", notes_figure:"1344ノーツ", time:"162秒", notes_density:"8.30ノーツ/秒", image:"ATHAZA.webp"},
+  { name:"Avataar ~Reincarnation of Kalpa~", level:16.6, author_music:"Scarlette a.k.a. CrYmson", author_notes:"XMT & Salt", bpm:"180bpm", notes_figure:"1074ノーツ", time:"174秒", notes_density:"6.17ノーツ/秒", image:"Avataar.webp"},
+  { name:"PRAGMATISM -RESURRECTION-", level:16.6, author_music:"Laur", author_notes:"THE LAST 198", bpm:"174bpm", notes_figure:"1156ノーツ", time:"156秒", notes_density:"7.41ノーツ/秒", image:"PRAGMATISM -RESURRECTION-.jpeg"},
+  { name:"volcanic", level:16.6, author_music:"DETRO a.k.a. ルゼ", author_notes:"Alloces", bpm:"191~382bpm", notes_figure:"1650ノーツ", time:"137秒", notes_density:"12.04ノーツ/秒", image:"volcanic.webp"},
+  { name:"Ark", level:16.5, author_music:"kanoryo", author_notes:"StupiGGy", bpm:"180bpm", notes_figure:"1091ノーツ", time:"163秒", notes_density:"6.69ノーツ/秒", image:"Ark.webp"},
+  { name:"Indelible Scar", level:16.5, author_music:"Noah", author_notes:"上班睡大觉_Sleepyhead", bpm:"223bpm", notes_figure:"1207ノーツ", time:"140秒", notes_density:"8.62ノーツ/秒", image:"Indelible_Scar.webp"},
+  { name:"Lyrith -迷宮リリス-", level:16.5, author_music:"ユメミド", author_notes:"CN_115 as \"prediCameNt\"", bpm:"177bpm", notes_figure:"1122ノーツ", time:"147秒", notes_density:"7.63ノーツ/秒", image:"Lyrith.webp"},
+  { name:"Antithese", level:16.4, author_music:"Blacklolita", author_notes:"弧光", bpm:"172bpm", notes_figure:"1200ノーツ", time:"144秒", notes_density:"8.33ノーツ/秒", image:"Antithese.jpeg"},
+  { name:"PANIC PARADISE", level:16.4, author_music:"DJ SHION.Y", author_notes:"StR-1", bpm:"170bpm", notes_figure:"1111ノーツ", time:"128秒", notes_density:"8.68ノーツ/秒", image:"PANIC_PARADISE.jpg"},
+  { name:"狂喜蘭舞", level:16.4, author_music:"LeaF", author_notes:"Catcats a.k.a. 無極 vs. H.C.C. fixed by 野从&晨", bpm:"230bpm", notes_figure:"954ノーツ", time:"162秒", notes_density:"5.89ノーツ/秒", image:"狂喜蘭舞.webp"},
+  { name:"S.A.T.E.L.L.I.T.E.", level:16.2, author_music:"かめりあ", author_notes:"N.E.R.1.9.8.", bpm:"150bpm", notes_figure:"1098ノーツ", time:"190秒", notes_density:"5.78ノーツ/秒", image:"sat.webp"},
+  { name:"Shadow", level:16.4, author_music:"姜米條&SumaiLight", author_notes:"J0(_)I2|\|3y'5 3|\||)", bpm:"172bpm", notes_figure:"1089ノーツ", time:"143秒", notes_density:"7.62ノーツ/秒", image:"Shadow.webp"},
+  { name:"Ποσειδών", level:16.4, author_music:"1112 vs. Star*", author_notes:"上班摸大鱼 a.k.a. 海洋咩兔", bpm:"175bpm", notes_figure:"1228ノーツ", time:"123秒", notes_density:"9.98ノーツ/秒", image:"Poseidon.webp"},
+  { name:"Cthugha", level:16.3, author_music:"USAO", author_notes:"百九十八 feat. NerSAN", bpm:"213bpm", notes_figure:"1444ノーツ", time:"154秒", notes_density:"9.38ノーツ/秒", image:"Cthugha.webp"},
+  { name:"TECHNOPOLIS 2085", level:15.9, author_music:"PRASTIK DANCEFLOOR", author_notes:"TECHNOXMT vs LATENCYPOLIS", bpm:"134bpm", notes_figure:"824ノーツ", time:"140秒", notes_density:"5.89ノーツ/秒", image:"TECHNOPOLIS 2085.webp"},
+  { name:"You are the Miserable", level:15.8, author_music:"t+pazolite", author_notes:"Pat Brick Man Mr.C & Mr.B", bpm:"163bpm", notes_figure:"1344ノーツ", time:"164秒", notes_density:"8.20ノーツ/秒", image:"miserable.webp"},
+  { name:"Credits", level:15.7, author_music:"Frums", author_notes:"_鉄(Isotope 59)", bpm:"179bpm", notes_figure:"638ノーツ", time:"101秒", notes_density:"6.32ノーツ/秒", image:"credits.png"},
+  { name:"月詠に鳴る", level:15.7, author_music:"Feryquitous feat. 藍月なくる", author_notes:"StupiGGy", bpm:"180bpm", notes_figure:"938ノーツ", time:"135秒", notes_density:"6.95ノーツ/秒", image:"Tsukuyomi.webp"},
+  { name:"Radiance", level:15.5, author_music:"Nhato", author_notes:"Яadiance", bpm:"155bpm", notes_figure:"926ノーツ", time:"147秒", notes_density:"6.30ノーツ/秒", image:"Radiance.webp"},
+  { name:"もぺもぺ", level:15.4, author_music:"LeaF", author_notes:"阿爽", bpm:"100bpm", notes_figure:"720ノーツ", time:"110秒", notes_density:"6.55ノーツ/秒", image:"もぺもぺ.webp"},
+  { name:"风屿", level:15.1, author_music:"闫东炜", author_notes:"JKy", bpm:"170bpm", notes_figure:"1236ノーツ", time:"179秒", notes_density:"6.91ノーツ/秒", image:"风屿.webp"},
+  { name:"Break Over", level:14.9, author_music:"K-forest", author_notes:"百九十八", bpm:"185bpm", notes_figure:"570ノーツ", time:"128秒", notes_density:"4.45ノーツ/秒", image:"Break_Over.webp"},
+  { name:"雪降り ~雪が降っている~", level:14.0, author_music:"AiSS w/ 夜輪 ft. 結月ゆかり", author_notes:"小咩兔的多指课堂 vs. 上课睡大觉", bpm:"156bpm", notes_figure:"531ノーツ", time:"151秒", notes_density:"3.52ノーツ/秒", image:"Yukihuri.webp"},
+  { name:"dB doll", level:13.5, author_music:"YUE.STEVEN/uen", author_notes:"NerSAN & 百九十八", bpm:"128bpm", notes_figure:"377ノーツ", time:"108秒", notes_density:"3.49ノーツ/秒", image:"dbdoll.webp"},
+];
+
+app.get("/phigros", (req, res) => {
+  res.render('phigros', { data: phigros });
+});
+
+app.get("/phigros/create", (req, res) => {
+  res.redirect('/public/phigros_new.html');
+});
+
+app.post("/phigros", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  const name = req.body.name;
+  const author_music = req.body.author_music;
+  const author_notes = req.body.author_notes;
+  const level = req.body.level;
+  const bpm = req.body.bpm;
+  const notes_figure = req.body.notes_figure;
+  const notes_density = req.body.notes_density;
+  const time = req.body.time;
+  const image = req.body.image;
+  phigros.push( { name: name, author_music: author_music, author_notes: author_notes, level: level, bpm: bpm, notes_figure: notes_figure, notes_density: notes_density, time: time, image: image } );
+  console.log( phigros );
+  res.render('phigros', {data: phigros} );
+});
+
+app.get("/phigros/:number", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  const number = req.params.number;
+  const detail = phigros[ number ];
+  res.render("phigros_detail", {id: number, data: detail, icon: { filename: "/public/" + detail.image, alt: detail.name + " jacket" }});
+});
+
+app.get("/phigros/edit/:number", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  const number = req.params.number;
+  const detail = phigros[ number ];
+  res.render('phigros_edit', {id: number, data: detail} );
+});
+
+app.post("/phigros/update/:number", (req, res) => {
+  // 本来は変更する番号が存在するか，各項目が正しいか厳重にチェックする
+  // 本来ならここにDBとのやり取りが入る
+  phigros[req.params.number].name = req.body.name;
+  phigros[req.params.number].author_music = req.body.author_music;
+  phigros[req.params.number].author_notes = req.body.author_notes;
+  phigros[req.params.number].level = req.body.level;
+  phigros[req.params.number].bpm = req.body.bpm;
+  phigros[req.params.number].notes_figure = req.body.notes_figure;
+  phigros[req.params.number].notes_density = req.body.notes_density;
+  phigros[req.params.number].time = req.body.time;
+  phigros[req.params.number].image = req.body.image;
+  console.log( phigros );
+  res.redirect('/phigros' );
+});
+
+app.get("/phigros/delete/:number", (req, res) => {
+  // 本来は削除の確認ページを表示する
+  // 本来は削除する番号が存在するか厳重にチェックする
+  // 本来ならここにDBとのやり取りが入る
+  const number = req.params.number;
+  const detail = phigros[ number ];
+  res.render("phigros_delete_check", {id: number, data: detail});
+});
+
+app.post("/phigros/delete/:number", (req, res) => {
+  const number = req.params.number;
+  phigros.splice(number, 1); // ← ここで初めて削除
+  res.redirect("/phigros");
+});
+
+app.use(function(req, res, next) {
+    res.status(404).send('ページが見つかりません');
+});
+
+app.listen(8080, () => console.log("Example app listening on port 8080!"));
--- spec.md
+++ spec.md
@@ -10,11 +10,11 @@ name|text|曲名
 author_music|text|曲作者名
 author_notes|text|譜面作者名
 level|number(step:0.1)|難易度
-score|number|最高スコア
 bpm|number(step:0.1)|速度
 notes_figure|number|ノーツ数
 notes_density|number(step:0.1)|ノーツ密度
 time|number|曲の時間
+image|image|ジャケット絵
 
 ### ページ構造
 
@@ -23,9 +23,9 @@ time|number|曲の時間
 report.js|-|Webサーバプログラム
 phigros.ejs|GET|一覧表示
 phigros_detail.ejs|GET|詳細表示
-phigros_add.ejs|POST|追加画面
-phigros_edit.ejs|PUT|編集画面
-phigros_delete.ejs|DELETE|削除画面
+phigros_edit.ejs|POST|編集画面
+phigros_delete_check.ejs|GET|削除確認画面
+phigros_new.html|POST|曲追加画面
 
 ### ページ遷移
 
@@ -33,87 +33,112 @@ phigros_delete.ejs|DELETE|削除画面
 stateDiagram-v2
     [*] --> 一覧表示
     一覧表示 --> 詳細表示: 曲名からリンク
-    詳細表示 --> 編集画面: 編集ボタン
-    編集画面 --> 登録
-    登録 --> 一覧表示
-    一覧表示 --> 追加画面: 追加ボタン
-    追加画面 --> 登録
-    詳細表示 --> 削除画面: 削除ボタン
-    削除画面 --> 一覧表示
+    詳細表示 --> 編集画面: 「編集」からリンク
+    編集画面 --> 編集処理: 登録ボタン
+    編集処理 --> 一覧表示
+    編集画面 --> 詳細表示: 「詳細に戻る」からリンク
+    編集画面 --> 一覧表示: 「曲一覧に戻る」からリンク
+    詳細表示 --> 一覧表示: 「曲一覧に戻る」からリンク
+    一覧表示 --> 曲追加画面: 「追加」からリンク
+    曲追加画面 --> 登録処理: 登録ボタン
+    登録処理 --> 一覧表示
+    曲追加画面 --> 一覧表示: 「曲一覧に戻る」からリンク
+    詳細表示 --> 削除確認画面: 「削除」からリンク
+    削除確認画面 --> 削除処理:「はい、削除します」ボタン
+    削除処理 --> 一覧表示
+    削除確認画面 --> 詳細表示:「いいえ(詳細に戻る)」からリンク
 ```
 
-## 2:物価上昇率が急激な食品集計システム
+## 2:wtt（world table tennis） 男子選手データベース
 
 ### データ構造
 
 項目名 | 型 | 内容
 -|-|-
-name|text|食品名
-cost|number|値段
-uprate|number(step:0.1)|昨年比の値段上昇率
-production|number|生産数
+name|text|選手名
+country|text|出身国
+age|number|年齢
+world_ranking|number|世界ランキング
+result|number|過去の成績
+hand|text|利き手
+type|text|戦型（ラケット）
 
 ### ページ構造
 
 ページ（リソース）名|メソッド|役割
 -|-|-
 report.js|-|Webサーバプログラム
-foodcost.ejs|GET|一覧表示
-foodcost_detail.ejs|GET|詳細表示
-foodcost_add.ejs|POST|追加画面
-foodcost_edit.ejs|PUT|編集画面
-foodcost_delete.ejs|DELETE|削除画面
+wtt.ejs|GET|一覧表示
+wtt_detail.ejs|GET|詳細表示
+wtt_edit.ejs|POST|編集画面
+wtt_delete_check.ejs|GET|削除確認画面
+wtt_new.html|POST|選手情報追加画面
 
 ### ページ遷移
 
 ```mermaid
 stateDiagram-v2
     [*] --> 一覧表示
-    一覧表示 --> 詳細表示: 曲名からリンク
-    詳細表示 --> 編集画面: 編集ボタン
-    編集画面 --> 登録
-    登録 --> 一覧表示
-    一覧表示 --> 追加画面: 追加ボタン
-    追加画面 --> 登録
-    詳細表示 --> 削除画面: 削除ボタン
-    削除画面 --> 一覧表示
+    一覧表示 --> 詳細表示: 選手名からリンク
+    詳細表示 --> 編集画面: 「編集」からリンク
+    編集画面 --> 編集処理: 登録ボタン
+    編集処理 --> 一覧表示
+    編集画面 --> 詳細表示: 「詳細に戻る」からリンク
+    編集画面 --> 一覧表示: 「選手一覧に戻る」からリンク
+    詳細表示 --> 一覧表示: 「選手一覧に戻る」からリンク
+    一覧表示 --> 選手情報追加画面: 「追加」からリンク
+    選手情報追加画面 --> 登録処理: 登録ボタン
+    登録処理 --> 一覧表示
+    選手情報追加画面 --> 一覧表示: 「選手一覧に戻る」からリンク
+    詳細表示 --> 削除確認画面: 「削除」からリンク
+    削除確認画面 --> 削除処理:「はい、削除します」ボタン
+    削除処理 --> 一覧表示
+    削除確認画面 --> 詳細表示:「いいえ(詳細に戻る)」からリンク
 ```
 
-## 3:カラオケ人気曲集計システム
+## 3:deltarune 消費アイテム一覧
 
 ### データ構造
 
 項目名 | 型 | 内容
 -|-|-
-name|text|曲名
-author|text|作曲者
-debut|number|公開日
-time|number|再生時間
-tone|text|キー
-popularity|number|月間再生回数
+name|text|アイテム名
+chapter|text|登場チャプター
+recover|number|回復量
+describe|text|アイテム説明
+place|text|入手方法（場所）
+plus_alpha|text|補足（特性など）
+image|image|アイテムの絵
 
 ### ページ構造
 
 ページ（リソース）名|メソッド|役割
 -|-|-
 report.js|-|Webサーバプログラム
-karaoke.ejs|GET|一覧表示
-karaoke_detail.ejs|GET|詳細表示
-karaoke_add.ejs|POST|追加画面
-karaoke_edit.ejs|PUT|編集画面
-karaoke_delete.ejs|DELETE|削除画面
+deltarune.ejs|GET|一覧表示
+deltarune_detail.ejs|GET|詳細表示
+deltarune_edit.ejs|POST|編集画面
+deltarune_delete_check.ejs|GET|削除確認画面
+deltarune_new.html|POST|アイテム情報追加画面
 
 ### ページ遷移
 
 ```mermaid
 stateDiagram-v2
     [*] --> 一覧表示
-    一覧表示 --> 詳細表示: 曲名からリンク
-    詳細表示 --> 編集画面: 編集ボタン
-    編集画面 --> 登録
-    登録 --> 一覧表示
-    一覧表示 --> 追加画面: 追加ボタン
-    追加画面 --> 登録
-    詳細表示 --> 削除画面: 削除ボタン
-    削除画面 --> 一覧表示
+    一覧表示 --> 詳細表示: アイテム名からリンク
+    詳細表示 --> 編集画面: 「編集」からリンク
+    編集画面 --> 編集処理: 登録ボタン
+    編集処理 --> 一覧表示
+    編集画面 --> 詳細表示: 「詳細に戻る」からリンク
+    編集画面 --> 一覧表示: 「アイテム一覧に戻る」からリンク
+    詳細表示 --> 一覧表示: 「アイテム一覧に戻る」からリンク
+    一覧表示 --> アイテム情報追加画面: 「追加」からリンク
+    アイテム情報追加画面 --> 登録処理: 登録ボタン
+    登録処理 --> 一覧表示
+    アイテム情報追加画面 --> 一覧表示: 「アイテム一覧に戻る」からリンク
+    詳細表示 --> 削除確認画面: 「削除」からリンク
+    削除確認画面 --> 削除処理:「はい、削除します」ボタン
+    削除処理 --> 一覧表示
+    削除確認画面 --> 詳細表示:「いいえ(詳細に戻る)」からリンク
 ```
